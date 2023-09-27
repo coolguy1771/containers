0dc69559ef2c6deb5c55a22e6f2890619ee5bc77
Tyler Witlin <twitlin@witl.xyz>
2023-09-27 11:06:13 -0400

    fix: strip quotes in jq
    Signed-off-by: Tyler Witlin <twitlin@witl.xyz>

--- apps/se-exporter/ci/latest.sh
+++ apps/se-exporter/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-version=$(curl -L -s curl -XGET -H "Accept: application/vnd.github+json"  -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/akimrx/space-engineers-exporter/releases | jq .[0].tag_name| tr -d '"')
+version=$(curl -L -s curl -XGET -H "Accept: application/vnd.github+json"  -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/akimrx/space-engineers-exporter/releases | jq -r .[0].tag_name)
 printf "%s" "${version}"
