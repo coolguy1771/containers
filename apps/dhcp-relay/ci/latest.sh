e4435876b3cc668312d2dc64073bf304d89ab8bc
Tyler Witlin <twitlin@witl.xyz>
2024-09-23 08:53:38 -0400

    fix: set tag to sha
    Signed-off-by: Tyler Witlin <twitlin@witl.xyz>

--- apps/dhcp-relay/ci/latest.sh
+++ apps/dhcp-relay/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/ipxe/ipxe/tags" | jq --raw-output '.[0].name' 2>/dev/null)
+version=$(curl -sX GET -H "Accept: application/vnd.github.VERSION.sha" "https://api.github.com/repos/ipxe/ipxe/commits/master" 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
